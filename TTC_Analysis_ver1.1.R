213f1cceaa5cc25583b794bf9547f8b1c13ec560
Amrkenawy <Amrkenawy@users.noreply.github.com>
2018-10-08 17:38:34 -0400

    a

--- TTC_Analysis_ver1.1.R
+++ TTC_Analysis_ver1.1.R
@@ -34,12 +34,12 @@ for (b in c(14,15,16,17,18)){
 write.csv(df,"D:/DataAnalytics/CKME_136/TTC_Bus_Delay/Capstone/combined.csv")
 
 
-#library(readxl)
-#library(stringr)
-#library(ggplot2)
+library(readxl)
+library(stringr)
+library(ggplot2)
 
 
-#df <- read.table("D:/DataAnalytics/DataSets/test/combined.csv", header = TRUE, stringsAsFactors = FALSE, sep = ",")
+df <- read.csv("D:/DataAnalytics/CKME_136/TTC_Bus_Delay/Capstone/combined.csv", header = TRUE, stringsAsFactors = FALSE, sep = ",")
 
 head(df)
 
